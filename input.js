d6cc897d4998b79c853082c4943d037656ac9f2c
andredecre <73247775+andredecre@users.noreply.github.com>
2021-01-04 20:51:45 +0100

    Fix login bug

--- input.js
+++ input.js
@@ -3,10 +3,8 @@ $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
     
     //called when key is pressed in textbox
-    $("#ccnumber, #cvv, #postCode").keypress(function (e) {
-       //if the letter is not digit then display error and don't type anything
+    $("#ccnumber, #cvv, #postCode, #piva, #phonenumber").keypress(function (e) {
         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-          //display error message
         return false;
         };
     });
--- template/template-subscribe.php
+++ template/template-subscribe.php
@@ -23,48 +23,48 @@
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
                     <label for="name" class="col-11 text-left mt-2">Nome:</label>
-                    <input type="text" class="form-control col-11" name="name" aria-describedby="" placeholder="Inserisci nome" required>
+                    <input type="text" class="form-control col-11" id="name" name="name" placeholder="Inserisci nome" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['name-error']))echo$_POST['name-error'];?></label>
                 </div>
                 <div class="form-group col-md-6 my-4">
                     <label for="surname" class="col-11 text-left mt-2">Cognome:</label>
-                    <input type="text" class="form-control col-11" name="surname" aria-describedby="" placeholder="Inserisci cognome" required>
+                    <input type="text" class="form-control col-11" id="surname" name="surname" placeholder="Inserisci cognome" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['surname-error']))echo$_POST['surname-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
                     <label for="birthdate" class="col-11 text-left mt-2">Data di nascita:</label>
-                    <input type="date" class="form-control col-11" name="birthdate" aria-describedby="">
+                    <input type="date" class="form-control col-11" id="birthdate" name="birthdate" aria-describedby="">
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['birthdate-error']))echo$_POST['birthdate-error'];?></label>
                 </div>
                 <div class="form-group col-md-6 my-4">
-                    <label for="phone-number" class="col-11 text-left mt-2">Telefono:</label>
-                    <input type="tel" class="form-control col-11" name="phone-number" min="10" max="10" aria-describedby="" placeholder="Inserisci numero di telefono" required>
+                    <label for="phone_number" class="col-11 text-left mt-2">Telefono:</label>
+                    <input type="text" class="form-control col-11" id="phone_number" name="phone_number" minlength="10" maxlength="10" aria-describedby="" placeholder="Inserisci numero di telefono" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['phone-error']))echo$_POST['phone-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
                     <label for="email" class="col-11 text-left mt-2">Email:</label>
-                    <input type="email" class="form-control col-11" name="email" aria-describedby="emailHelp" placeholder="Inserisci email" required>
+                    <input type="email" class="form-control col-11" id="email" name="email" aria-describedby="emailHelp" placeholder="Inserisci email" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['email-error']))echo$_POST['email-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
-                    <label for="password"  class="col-11 text-left mt-2">Password:</label>
-                    <input type="password" class="form-control col-11" name="password" min="8" placeholder="Inserisci password" required>
+                    <label for="password"  class="col-11 text-left mt-2">Password (minimo 8 caratteri):</label>
+                    <input type="password" class="form-control col-11" id="password" name="password" placeholder="Inserisci password" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['password-error']))echo$_POST['password-error'];?></label>
                 </div>
                 <div class="form-group col-md-6 my-4">
-                    <label for="repeat-password" class="col-11 text-left mt-2">Ripeti password:</label>
-                    <input type="password" class="form-control col-11" name="repeat-password" min="8" aria-describedby="" placeholder="Inserisci password" required>
+                    <label for="repeat_password" class="col-11 text-left mt-2">Ripeti password:</label>
+                    <input type="password" class="form-control col-11" id="repeat_passowrd" name="repeat_password" aria-describedby="" placeholder="Inserisci password" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['password-error']))echo$_POST['password-error'];?></label>
                 </div>
             </div>
             <div class="form-group text-right mt-3">
-                <input type="text" style="display:none;" name="subscriptionType" value="customer">
+                <input type="hidden" name="subscriptionType" value="customer">
                 <button type="submit" name="fields"  value="true" class="btn btn-light">Invia</button>
             </div>
         </form>
@@ -82,43 +82,43 @@ Form for Seller!
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
                     <label for="name" class="col-11 text-left mt-2">Nome:</label>
-                    <input type="text" class="form-control col-11" name="name" aria-describedby="" placeholder="Inserisci nome" required>
+                    <input type="text" class="form-control col-11" id="name" name="name" aria-describedby="" placeholder="Inserisci nome" required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['name-error']))echo$_POST['name-error'];?></label>
                 </div>
                 <div class="form-group col-md-6 my-4">
-                    <label for="surname" class="col-11 text-left mt-2">P.IVA:</label>
-                    <input type="text" class="form-control col-11" name="p_iva" aria-describedby="" min="11" max="11" placeholder="00000000000" required>
-                    <label for="surname"  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['p_iva-error']))echo$_POST['p_iva-error'];?></label>
+                    <label for="p_iva" class="col-11 text-left mt-2">P.IVA:</label>
+                    <input type="text" class="form-control col-11" id="p_iva" name="p_iva" aria-describedby="" minlength="11" maxlength="11" placeholder="00000000000" required>
+                    <label class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['p_iva-error']))echo$_POST['p_iva-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
-                    <label for="phone-number" class="col-11 text-left mt-2">Telefono:</label>
-                    <input type="tel" class="form-control col-11" name="phone-number" min="10" max="10" aria-describedby="" placeholder="Inserisci numero di telefono" required>
-                    <label for="phone-number"  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['phone-error']))echo$_POST['phone-error'];?></label>
+                    <label for="phone_number" class="col-11 text-left mt-2">Telefono:</label>
+                    <input type="text" class="form-control col-11" id="phone_number" name="phone_number" minlength="10" maxlength="10" aria-describedby="" placeholder="Inserisci numero di telefono" required>
+                    <label for="" class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['phone-error']))echo$_POST['phone-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
                     <label for="email" class="col-11 text-left mt-2">Email:</label>
-                    <input type="email" class="form-control col-11" name="email" aria-describedby="emailHelp" placeholder="Inserisci email" required>
+                    <input type="email" class="form-control col-11" id="email" name="email" aria-describedby="emailHelp" placeholder="Inserisci email" required>
                     <label for="email"  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['email-error']))echo$_POST['email-error'];?></label>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 my-4">
-                    <label for="password"  class="col-11 text-left mt-2">Password:</label>
-                    <input type="password" class="form-control col-11" name="password" min="8" placeholder="Inserisci password"required>
+                    <label for="password"  class="col-11 text-left mt-2">Password (minimo 8 caratteri):</label>
+                    <input type="password" class="form-control col-11" id="password" name="password" placeholder="Inserisci password"required>
                     <label for=""  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['password-error']))echo$_POST['password-error'];?></label>
                 </div>
                 <div class="form-group col-md-6 my-4">
-                    <label for="repeat-password" class="col-11 text-left mt-2">Ripeti password:</label>
-                    <input type="password" class="form-control col-11" name="repeat-password" min="8" aria-describedby="" placeholder="Inserisci password" required>
+                    <label for="repeat_password" class="col-11 text-left mt-2">Ripeti password:</label>
+                    <input type="password" class="form-control col-11" id="repeat_password" name="repeat_password" aria-describedby="" placeholder="Inserisci password" required>
                     <label for="repeat-password"  class="col-11 text-left mt-2 error-label"><?php if(isset($_POST['password-error']))echo$_POST['password-error'];?></label>
                 </div>
             </div>
             <div class="form-group text-right mt-3">
-                <input type="text" style="display:none;" name="subscriptionType" value="seller">
+                <input type="hidden" name="subscriptionType" value="seller">
                 <button type="submit" name="fields" value="true" class="btn btn-light">Invia</button>
             </div>
         </form>
