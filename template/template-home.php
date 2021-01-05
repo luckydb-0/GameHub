65ff79676e19ad4a3fabf87be7b785f895bb793a
andredecre <73247775+andredecre@users.noreply.github.com>
2021-01-05 20:00:19 +0100

    Last Release container has now fixed dimensions not depending on image heights

--- template/template-home.php
+++ template/template-home.php
@@ -4,9 +4,8 @@
     </div>
 </div>
 <div class="row justify-content-center mx-0">
-    <div class="container bg-dark py-3">
-        <!-- Fare altro carousel per schermi piccoli-->
-        <div id="ultime-uscite" class="carousel slide" data-ride="carousel">
+    <div class="container bg-dark py-4 pt-lg-5 rounded">
+        <div id="lastReleased" class="carousel slide" data-ride="carousel">
             <?php 
                 $n = 6;
                 $values = $dbr->getNewestGames($n);
@@ -17,21 +16,21 @@
                  <?php   
                     for($current = 0; $current < 3; $current++): 
                 ?>
-                <li data-target="#ultime-uscite" data-slide-to="<?php echo $current; ?>" <?php if($current == 0) echo 'class="active"'; ?> ></li>
+                <li data-target="#lastReleased" data-slide-to="<?php echo $current; ?>" <?php if($current == 0) echo 'class="active"'; ?> ></li>
                 <?php endfor; ?>
             </ul>
         
             <!-- The slideshow -->
-            <div class="carousel-inner my-3">
+            <div class="carousel-inner my-auto">
                 <?php
                     $isFirst = true;
                     for($counter = 0; $counter < $n; $counter++):
                         $game = $values[$counter];
                 ?>
-                <div class="carousel-item <?php if($isFirst){ echo "active"; $isFirst = !$isFirst;}; ?>">
-                    <div class="row text-center justify-content-center">
-                        <div class="col-md-5 my-auto text-center">
-                            <div>
+                <div class="carousel-item my-auto <?php if($isFirst){ echo "active"; $isFirst = !$isFirst;}; ?>">
+                    <div class="row text-center my-auto justify-content-center">
+                        <div class="col-12 col-md-5 my-auto text-center">
+                            <div class="my-auto">
                                 <a href="article.php?game=<?php echo $game["gameId"]; ?>">
                                     <img src="<?php echo IMG_DIR.$game["image"]; ?>" class="img-thumbnail">
                                     <figcaption><?php echo $game["title"]; ?></figcaption>
@@ -39,8 +38,8 @@
                             </div>
                         </div>
                         <?php $game = $values[++$counter]; ?>
-                        <div class="col-md-5 my-auto text-center">
-                            <div>
+                        <div class="col-12 col-md-5 my-auto text-center">
+                            <div class="my-auto">
                                 <a href="article.php?game=<?php echo $game["gameId"]; ?>">
                                     <img src="<?php echo IMG_DIR.$game["image"]; ?>" class="img-thumbnail">
                                     <figcaption><?php echo $game["title"]; ?></figcaption>
@@ -53,10 +52,10 @@
             </div>
         
             <!-- Left and right controls -->
-            <a class="carousel-control-prev" href="#ultime-uscite" data-slide="prev">
+            <a class="carousel-control-prev" href="#lastReleased" data-slide="prev">
             <span class="carousel-control-prev-icon"></span>
             </a>
-            <a class="carousel-control-next" href="#ultime-uscite" data-slide="next">
+            <a class="carousel-control-next" href="#lastReleased" data-slide="next">
             <span class="carousel-control-next-icon"></span>
             </a>
         
@@ -64,7 +63,7 @@
     </div>
 </div>
         <div class="row mx-0">
-            <div class="col-*-12 m-3">
+            <div class="col-*-12 mt-3 ml-4">
                 <h2>Preordina</h2>
             </div>
         </div>
@@ -82,7 +81,7 @@
             <?php endforeach; ?>
         </div>
         <div class="row mx-0">
-            <div class="col-*-12 m-3">
+            <div class="col-*-12 mt-3 ml-4">
                 <h2>I più venduti</h2>
             </div>
         </div>
