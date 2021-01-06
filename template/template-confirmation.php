4e057ef1847c89ece6415e2133636210caa11d51
Gianluca De Bonis <gianluca.debonis3@studio.unibo.it>
2021-01-06 22:24:21 +0100

    Add activate page

---
+++ template/template-confirmation.php
@@ -0,0 +1,6 @@
+<div class="row m-5 text-center">
+    <div class="col-11 col-sm-10 p-3 p-sm-5 bg-dark rounded offset-sm-1">
+        
+        <h2>Account attivato correttamente.</h2>
+    </div>
+</div>
