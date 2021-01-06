a13e971727fd036d3ec1747c1687fff3e36ed555
Gianluca De Bonis <gianluca.debonis3@studio.unibo.it>
2021-01-06 23:10:02 +0100

    Delete useless files

--- recover_password.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'bootstrap.php';
-
-$templateParams["title"] = "GameHub - Password Recovery";
-$templateParams["name"] = "template/template-recover-password.php";
-
-require 'template/base.php';
-
